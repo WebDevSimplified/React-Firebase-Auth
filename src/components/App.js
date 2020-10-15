e1db1e7637e78b448b59b7ec7ea4260c0e59bd4c
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-10-15 14:19:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
-import React from "react"
-import Signup from "./Signup"
-import { Container } from "react-bootstrap"
-import { AuthProvider } from "../contexts/AuthContext"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Dashboard from "./Dashboard"
-import Login from "./Login"
-import PrivateRoute from "./PrivateRoute"
-import ForgotPassword from "./ForgotPassword"
-import UpdateProfile from "./UpdateProfile"
+import React from "react";
+import Signup from "./Signup";
+import { Container } from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
+import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
 
 function App() {
   return (
@@ -32,4 +32,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
