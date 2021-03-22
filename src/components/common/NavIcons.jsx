46ed59f077684dff61d509b63411495b81987cf9
harsh-singhal7385 <harsh-singhal7385@github.com>
2021-03-22 21:32:19 +0530

    navicons css done

--- src/components/common/NavIcons.jsx
+++ src/components/common/NavIcons.jsx
@@ -3,16 +3,12 @@ import { FaCalendar } from 'react-icons/fa';
 import { Link } from 'react-router-dom'
 
 const NavIcons = () => {
-    const css = {
-      "line-height":"100px",
-      "color":"black",
-      "height": "20vh"
-    }
+
     return (
       <div className="sideBar">
-        <Link to="/profile"><div className="form-group" style={css}><FaCalendar size="30px"/></div></Link>
-        <Link to="/dashboard"><div className="form-group" style={css}><FaCalendar size="30px"/></div></Link>
-        <Link to="/archives"><div className="form-group" style={css}><FaCalendar size="30px"/></div></Link>
+        <Link to="/profile"><div className="form-group navicons"><FaCalendar className="navicons1"/></div></Link>
+        <Link to="/dashboard"><div className="form-group navicons"><FaCalendar className="navicons1"/></div></Link>
+        <Link to="/archives"><div className="form-group navicons"><FaCalendar className="navicons1"/></div></Link>
       </div>
     );
 }
