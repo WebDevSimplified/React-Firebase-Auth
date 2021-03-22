f6f9a5bac4ca3f05a14160f3b6daf08df5c132e7
harsh-singhal7385 <harsh-singhal7385@github.com>
2021-03-22 22:30:58 +0530

    navicons  css done

--- src/components/common/NavIcons.jsx
+++ src/components/common/NavIcons.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { FaCalendar } from 'react-icons/fa';
 import { Link } from 'react-router-dom'
+import '../css/Component.css'
 
 const NavIcons = () => {
 
     return (
-      <div className="sideBar">
+      <div className="">
         <Link to="/profile"><div className="form-group navicons"><FaCalendar className="navicons1"/></div></Link>
         <Link to="/dashboard"><div className="form-group navicons"><FaCalendar className="navicons1"/></div></Link>
         <Link to="/archives"><div className="form-group navicons"><FaCalendar className="navicons1"/></div></Link>
