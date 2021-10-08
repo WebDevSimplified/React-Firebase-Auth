65ac2bb3f944b4fcaaabbaeac906247713b81447
Marcos Antonio Gonçalves <marcos@compromitto.com.br>
2021-10-08 12:19:14 -0300

    Novas marcas
    Inclusão de icones e melhora no layout

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -3,6 +3,8 @@ import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link } from "react-router-dom"
 
+import logo from '../image/logo.svg'
+
 export default function ForgotPassword() {
   const emailRef = useRef()
   const { resetPassword } = useAuth()
@@ -18,9 +20,9 @@ export default function ForgotPassword() {
       setError("")
       setLoading(true)
       await resetPassword(emailRef.current.value)
-      setMessage("Check your inbox for further instructions")
+      setMessage("Verifique sua conta de e-mail e recupere sua senha")
     } catch {
-      setError("Failed to reset password")
+      setError("Falha na recuperação de senha")
     }
 
     setLoading(false)
@@ -28,27 +30,28 @@ export default function ForgotPassword() {
 
   return (
     <>
-      <Card>
+      <Card className="shadow p-3 mb-5 bg-white rounded">
         <Card.Body>
-          <h2 className="text-center mb-4">Password Reset</h2>
+        <img src={logo}alt="Gera pix" className="rounded mx-auto d-block mb-4" />
+          <h2 className="text-center mb-4">RECUPERAR A SENHA</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           {message && <Alert variant="success">{message}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
+              <Form.Control type="email" ref={emailRef} required placeholder="Digite seu e-mail"/>
             </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Reset Password
+            <Button disabled={loading} className="w-100 mt-4" type="submit">
+              RECUPERAR 
             </Button>
           </Form>
-          <div className="w-100 text-center mt-3">
-            <Link to="/login">Login</Link>
+          <div className="w-100 text-center mt-4">
+            <Link to="/login">ENTRAR NA CONTA</Link>
           </div>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
+        Crie uma conta! <Link to="/signup">CRIAR </Link>
       </div>
     </>
   )
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
-import React, { useRef, useState } from "react"
+import React, { useRef, useState,img} from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
+
+import logo from '../image/logo.svg'
 
 export default function Login() {
   const emailRef = useRef()
@@ -28,30 +30,31 @@ export default function Login() {
 
   return (
     <>
-      <Card>
+      <Card className="shadow p-3 mb-5 bg-white rounded">
         <Card.Body>
-          <h2 className="text-center mb-4">Log In</h2>
+        <img src={logo}alt="Gera pix" className="rounded mx-auto d-block mb-4" />
+          <h2 className="text-center mb-4 mt-4">ENTRAR NA CONTA</h2>
           {error && <Alert variant="danger">{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
+              <Form.Label className="mt-4">E-mail</Form.Label>
+              <Form.Control type="email" ref={emailRef} required placeholder="Digite seu e-mail" />
             </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
+            <Form.Group  className="mt-4" id="password">
+              <Form.Label>Senha</Form.Label>
+              <Form.Control className="form-control Default input" type="password" ref={passwordRef} required placeholder="Digite sua senha!" />
             </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Log In
+            <Button disabled={loading} className="w-100 mt-4" type="submit">
+              Entrar
             </Button>
           </Form>
           <div className="w-100 text-center mt-3">
-            <Link to="/forgot-password">Forgot Password?</Link>
+            <Link to="/forgot-password">Esqueceu sua senha?</Link>
           </div>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-        Need an account? <Link to="/signup">Sign Up</Link>
+        Crie uma conta! <Link to="/signup">Criar conta</Link>
       </div>
     </>
   )
