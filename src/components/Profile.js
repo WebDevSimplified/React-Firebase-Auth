7f17047e6ea1084d6836c6ef289bc7ccc07bc880
Marcos Antonio Gonçalves <58779341+compromitto@users.noreply.github.com>
2021-12-03 16:48:01 -0300

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -16,7 +16,7 @@ export default function Profile() {
   const [chave, setChave] = useState('');
   const [city, setCity] = useState('');
   const [name, setName] = useState('');
- 
+
   const { logout } = useAuth();
   const history = useHistory();
 
@@ -37,73 +37,85 @@ export default function Profile() {
     }
   }
   //Logout fim 
-  
+
   // Recuperara dados do realtime database 
   const chaveRef = firebase.database().ref();
 
   async function GetData() {
-    const chaveRes = await chaveRef.child(`clients/${user.uid}/key/`).get(user?.uid);
+    const chaveRes = await chaveRef.child(`clients/${user?.uid}/key/`).get(user?.uid);
+    
     const dataChave = [];
     const dataRes = chaveRes.val();
     for (let id in dataRes) {
       dataChave.push(id, dataRes);
     };
+    // ver dados do realtime database redirecionar para a pagina de valor
+
+  if (chaveRes.val() === null) {
+    history.push("/Creatkey")
+  } else {
+    history.push("/GerarValor")
+  }
+    // Fim do codigo redirecionamento
 
     const resChave = Object.entries(chaveRes.val() ?? {}).map(([key, value]) => {
       return {
         'chave': value.chave,
         'city': value.city,
         'name': value.name
-
+        
       }
+  
     }
+   
+    
     );
     setChave(resChave[0].chave);
     setCity(resChave[0].city);
     setName(resChave[0].name);
-    console.log(resChave[0].chave);
-    
+   
+
   };
+  GetData();
+  // fim de recuperar data
   
- GetData();
-    // fim de recuperar data
-    return (
-      <>
-        <Card className="text-light shadow  bg-secondary rounded mb-2">
-          <div className="w-100 text-right">
-            <Button className="mr-03 badge badge-secondary" variant="link" onClick={handleLogout}>
-              SAIR
-            </Button>
-          </div>
-          <Card.Body>
-            <img src={logo} alt="Gera pix" className="card-img-top mx-auto d-block mb-4" />
-            <h4 className="text-center mb-4 text-black">SUA CONTA</h4>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <p>Os dados que aparecem aqui são os dados que você cadastrou no seu perfil.
-            </p>
-          </Card.Body>
-        </Card>
-        <Card.Footer className="shadow p-3 mb-5 bg-dark text-white rounded p-3 mb-2">
-          <div className="user-info text-center mb-4">
-            <img className="rounded-circle text-center" src={user.photoURL} alt={user.displayName} />
-            <p className="font-weight-bold mb-4">{user.displayName}</p>
-          </div>
-          <div className="user-info">
-            <h4 className="text-center mb-4 mp-4">SUA CONTA PIX</h4>
-            <p className=" mb-0"> NOME:</p>
-            <p className="text-decoration-underline font-italic border-bottom "> {name}</p>
-            <p className=" mb-0"> CIDADE:</p>
-            <p className="text-decoration-underline font-italic border-bottom "> {city}</p>
-            <p className="mb-0"> CHAVE PIX:</p>
-            <p className="text-decoration-underline font-italic border-bottom "> {chave} </p>
-          </div>
-          <div className="pl-3 pr-3 row justify-content-between">
-            <Link className="btn btn-primary btn-sm" to="/UpData">Atualizar chave</Link>
-            <Link to="/update-profile" className="btn btn-primary btn-sm ">
-              Atualizar senha
-            </Link>
-          </div>
-        </Card.Footer>
-      </>
-    )
-  };
+  return (
+    <>
+      <Card className="text-light shadow  bg-secondary rounded mb-2">
+        <div className="w-100 text-right">
+          <Button className="mr-03 badge badge-secondary" variant="link" onClick={handleLogout}>
+            SAIR
+          </Button>
+        </div>
+        <Card.Body>
+          <img src={logo} alt="Gera pix" className="card-img-top mx-auto d-block mb-4" />
+          <h4 className="text-center mb-4 text-black">SUA CONTA</h4>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <p>Os dados que aparecem aqui são os dados que você cadastrou no seu perfil.
+          </p>
+        </Card.Body>
+      </Card>
+      <Card.Footer className="shadow p-3 mb-5 bg-dark text-white rounded p-3 mb-2">
+        <div className="user-info text-center mb-4">
+          <img className="rounded-circle text-center" src={user.photoURL} alt={user.displayName} />
+          <p className="font-weight-bold mb-4">{user.displayName}</p>
+        </div>
+        <div className="user-info">
+          <h4 className="text-center mb-4 mp-4">SUA CONTA PIX</h4>
+          <p className=" mb-0"> NOME:</p>
+          <p className="text-decoration-underline font-italic border-bottom "> {name}</p>
+          <p className=" mb-0"> CIDADE:</p>
+          <p className="text-decoration-underline font-italic border-bottom "> {city}</p>
+          <p className="mb-0"> CHAVE PIX:</p>
+          <p className="text-decoration-underline font-italic border-bottom "> {chave} </p>
+        </div>
+        <div className="pl-3 pr-3 row justify-content-between">
+          <Link className="btn btn-primary btn-sm" to="/UpData">Atualizar chave</Link>
+          <Link to="/update-profile" className="btn btn-primary btn-sm ">
+            Atualizar senha
+          </Link>
+        </div>
+      </Card.Footer>
+    </>
+  )
+};
