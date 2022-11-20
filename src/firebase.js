5b46f95a4c2749e182f42aed21c3370b3f5dfaa8
faynercosta <93364457+faynercosta@users.noreply.github.com>
2022-11-19 20:52:08 -0500

    Firebase Has changed
    I fixed the file for importing firebase SDK to this project.

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-const app = firebase.initializeApp({
+const app = initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASURAMENT_ID
 })
 
-export const auth = app.auth()
+export const auth = getAuth();
 export default app
